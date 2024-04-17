95e92be756735b51f36c13b8736dd6eb6ce0b1eb
katharinadanner <141484715+katharinadanner@users.noreply.github.com>
2024-04-17 17:07:41 +0200

    Added custom scrollbar, adapted colors

--- code.js
+++ code.js
@@ -1,29 +1,36 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var navbarCollapse = document.getElementById("mynavbar");
-    var navbarToggler = document.querySelector(".navbar-toggler");
-    var navLinks = document.querySelectorAll(".nav-link");
-  
-    // Load the JSON data
-    fetch('data.json')
-      .then(response => response.json())
-      .then(data => {
-        // Update navigation links with JSON data
-        data.pages.forEach((page, index) => {
-          navLinks[index].href = page.url;
-          navLinks[index].innerText = page.title;
-        });
-      })
-      .catch(error => console.error('Error fetching JSON:', error));
-  
-    navbarToggler.addEventListener("click", function() {
-      navbarCollapse.classList.toggle("show");
-      navbarToggler.classList.toggle("expanded");
-    });
-  
-    // Close navbar when a link is clicked (for mobile view)
-    navLinks.forEach(link => {
-      link.addEventListener("click", function() {
-        navbarCollapse.classList.remove("show");
+  var navbarCollapse = document.getElementById("mynavbar");
+  var navbarToggler = document.querySelector(".navbar-toggler");
+  var navLinks = document.querySelectorAll(".nav-link");
+
+  // Load the JSON data
+  fetch('data.json')
+    .then(response => response.json())
+    .then(data => {
+      // Update navigation links with JSON data
+      data.pages.forEach((page, index) => {
+        navLinks[index].href = page.url;
+        navLinks[index].innerText = page.title;
       });
+    })
+    .catch(error => console.error('Error fetching JSON:', error));
+
+  navbarToggler.addEventListener("click", function() {
+    navbarCollapse.classList.toggle("show");
+    navbarToggler.classList.toggle("expanded");
+  });
+
+  // Close navbar when a link is clicked (for mobile view)
+  navLinks.forEach(link => {
+    link.addEventListener("click", function() {
+      navbarCollapse.classList.remove("show");
     });
   });
+});
+
+// Adjust the content margin-top when the page is scrolled
+window.addEventListener('scroll', function() {
+  var navbar = document.querySelector('.navbar');
+  var content = document.querySelector('.content');
+  content.style.marginTop = navbar.offsetHeight + 'px';
+});
